a6323e8bea56ef27a35396ab51b2dcceec25a29d
niheper <159460032+tethranialn@users.noreply.github.com>
2024-11-15 21:14:55 +0300

    smth

--- prog5/main.cpp
+++ prog5/main.cpp
@@ -22,13 +22,13 @@ bool CalcSize(unsigned &a, unsigned *b, fstream& g, int& raz);
 bool InpF(unsigned a, unsigned b, float A[N][M], fstream &g);
 void Out(float A[N][M], unsigned a, unsigned b, fstream &g);
 void OutStroke(unsigned b, fstream &g, float A[M]);
-int Process(float A[N][M], unsigned a, unsigned b, int m, int k, float& result);
+float Process(float A[N][M], unsigned a, unsigned b, int m, int k, float& result);
 bool InM(int& m, unsigned a, fstream& g);
 bool InK(int& k, unsigned a, fstream& g);
 void main(void)
 {
 	setlocale(LC_ALL, "russian");
-	fstream g; float A[N][M], result = 0; unsigned a, b; char Name[20]; int m, k, raz = 0;
+	fstream g; float A[N][M], result = 0; unsigned a, b;int m, k, raz = 0;
 	g.open("out.txt", ios::out);
 	if (!g.is_open()) cout << "File is not opened\n";
 	else
@@ -113,7 +113,7 @@ void Out(float A[N][M], unsigned a, unsigned b, fstream& g)
 }
 bool CalcSize(unsigned& a, unsigned* b, fstream& g, int& raz)
 {
-	fstream f; unsigned h = 0, size = 0; char s = '!'; float tmp;
+	fstream f; unsigned h = 0, size = 0; char s = '!'; float tmp; int temp;
 	f.open("in.txt", ios::in);
 	if (!f.is_open())
 	{
@@ -123,19 +123,19 @@ bool CalcSize(unsigned& a, unsigned* b, fstream& g, int& raz)
 	}
 	else
 	{
-		f >> tmp;
-		if ((f.eof()) || (tmp <= 0))
+		f >> temp;
+		if ((f.eof()) || (temp <= 0))
 		{
 			a = 0;
 			*b = 0;
 			raz = a;
 		}
-		else if (tmp > N)
+		else if (temp > N)
 		{
 			a = N;
 			raz = a;
 		}
-		else a = tmp;
+		else a = temp;
 		raz = a;
 		*b = a;
 		while (!f.eof() && s != '\n')
@@ -222,7 +222,7 @@ bool InM(int& m,unsigned a, fstream& g)
 	}
 	else
 	{
-		//m--;
+		m--;
 		return true;
 	}
 }
@@ -242,22 +242,39 @@ bool InK(int& k, unsigned a, fstream& g)
 	}
 	else
 	{
-		//k--;
+		k--;
 		return true;
 	}
 }
-int Process(float A[N][M], unsigned a, unsigned b, int m, int k, float& result)
+float Process(float A[N][M], unsigned a, unsigned b, int m, int k, float& result)
 {
-	unsigned i = 0, j = m; float tmp, B[100]; int x = -1;
-	
-	for (i = 0; i < k; i++)
+	unsigned i = 0, j = m; int tmp1 = 0, tmp2 = 0, tmp3 = 0; float B[1000]; int x = -1;
+	do
 	{
-		for (j = m; j < a; j++)
+		for (i = 0+tmp1; i < k+1+tmp1; i++)
 		{
-			x++;
-			B[x] = A[i][j];
+			for (j = m - tmp2; j < a - tmp3; j++)
+			{
+				x++;
+				B[x] = A[i+tmp1][j];
+				cout << B[x] << " ";
+			}
+			cout << '\n';
 		}
-	}
+		if (tmp1 < a - k - 1)
+		{
+			tmp1 += k;
+		}
+		if (tmp2 != m)
+		{
+			tmp2++;
+			tmp3++;
+		}
+		else
+		{
+			tmp3++;
+		}
+	} while (i!=a);
 	/*
 	tmp = A[i][j];
 	for (x = 0; j > 0; x++)
